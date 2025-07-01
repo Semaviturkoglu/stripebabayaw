690e2812e2f7e25727911343cfbfb0e0d486e3c3
Semaviturkoglu <semaviturkoglu@gmail.com>
2025-07-01 18:57:58 +0300

    Update bot_token.py

--- bot_token.py
+++ bot_token.py
@@ -1,7 +1,7 @@
 # --- DOSYA: bot_token.py ---
 # Bütün gizli bilgiler burada.
 
-TELEGRAM_TOKEN = "7654849651:AAEO91DTf9vdpsbmerjEeBjTlR66rSkx5k8"
+TELEGRAM_TOKEN = "7654849651:AAHxOBCyVZCm4Ak45NJOmO5RFb5saRUYLLc"
 
 # Kurucu Komutan ID'si
 ADMIN_ID = 7272527047
